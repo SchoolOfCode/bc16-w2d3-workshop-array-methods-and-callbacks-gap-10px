29924f162befcc28b4247e293953dc42819790db
Gavin P <gavinapersad@gmail.com>
2024-02-14 16:23:49 +0000

    ticket 2 complete

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log ("Hello World");
+// - Create a function that accepts a name parameter
+
+function greet (name){
+    console.log (`Hello ${name}`);
+}
+
+// - It should log a greeting message using the name
