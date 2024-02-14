94a52f123547ab4ef8bfd7382b4eaf8d2bddb190
Pete Faretra <55142305+peterston-e@users.noreply.github.com>
2024-02-14 20:42:31 +0000

    start ticket 5

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function techWizzardry(callback, ...rest) {
   rest.map(callback);
 }
 
+let people = ["dave", "kevin", "simon", "Guillermo"];
+
+techWizzardry(greet, ...people);
+
 let productsArray = [
   {
     product: "Shoes",
@@ -39,3 +43,39 @@ let bigVatArray = productsArray.map((x) => {
   x.price *= 1.2;
   return x;
 });
+
+// ****** Ticket 4 *******
+
+// task 1
+let numArray = [21, 24, 14, 99, 109, 47];
+doubleNums = numArray.map((x) => x * 2);
+
+// task 2
+const words = ["spray", "elite", "exuberant", "destruction", "present"];
+let filterResult = words.filter((word) => word.length > 6);
+console.log(filterResult);
+
+// task 3
+const numbers = [3, 6, 2, 9, 1];
+const sum = numbers.reduce((p, c) => {
+  return p + c;
+}, 0);
+console.log(sum);
+
+// task 4
+const months = ["one", "two", "three", "five"];
+months.splice(3, 0, "plumb");
+// Inserts at index 3 and delets 0 off the end
+// .splice(3, 1) would delete the five.
+// inserts into the original array.
+console.log(months);
+
+// returns true or false
+console.log(months.includes("one"));
+
+// returns the first index at which a given element
+// can be found in the array, or -1 if it is not present.
+console.log(months.indexOf("plumb"));
+console.log(months.indexOf("foo"));
+
+// ***** Ticket 5 ********
